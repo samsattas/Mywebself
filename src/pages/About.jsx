af4768244f121ca8a4d6ff5968df361b62465818
Samuel Satizabal <samuelsatizabal@hotmail.com>
2023-11-28 00:27:26 -0500

    FEAT:about page almost ready

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,29 @@
+import profile from '../assets/Profile.png'
+
 const About = () => {
   return(
-    <div>about</div>
+    <div className='h-full bg-[#1B1B1E] pt-14 flex flex-col'>
+      <main className='sm:px-20 sm:pb-10 p-6 h-full flex flex-col'>
+        <article className='flex flex-col h-fit md:flex-row items-center gap-6 md:gap-16 bg-white rounded-xl p-10 pb-16'>
+          <h2 className='text-[4vh] text-center md:text-start font-bold w-full border-b-[#1B1B1E] border-b-2 block md:hidden'>About me</h2>
+          <img src={profile} className='rounded-full w-2/3 md:w-1/3 lg:w-1/4 md:rounded-xl md:drop-shadow-[30px_30px_1px_#A9BCD0]' />
+          <p className='text-justify text-[2.3vh] w-full'>
+            <h2 className='text-[4vh] text-center md:text-start font-bold w-full border-b-[#1B1B1E] border-b-2 hidden md:block'>About me</h2>
+            <br/>
+            I am a junior motivated Front-end Developer, certified as Software System Engineer, with 
+            expertise in <b>HTML, CSS, JavaScript, React, Sass,</b> and best practices such as <b>accessibility,
+            mobile-first, responsive design, atomic design and BEM</b>. Passionate about creating intuitive
+            and visually appealing user experiences. Strong problem-solving abilities and a detail-oriented
+            approach to deliver high-quality code. <br />
+            During my degree I had the opportunity to develop Backend projects, so I have knowledge in <b>design
+            patterns, UML diagrams, unit tests, integration tests</b> and in languages such as <b>Java, Javascript and C#</b>.<br />
+            I embrace my identity as a highly creative individual, constantly venturing beyond conventional 
+            boundaries. This distinctive perspective enables me to grasp the broader scope of situations and offer 
+            innovative ideas that were previously unexplored.
+          </p>
+        </article>
+      </main>
+    </div>
   )
 }
 
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -3,29 +3,28 @@ import landinImg1 from '../assets/landingImg1.png'
 
 const LandingPage = () => {
   return(
-    <div className='h-full bg-[#1B1B1E] pt-9 flex flex-col'>
-      <main className='sm:p-10 p-6 h-full'>
-        <article className='flex flex-col  sm:flex-row items-center bg-white rounded-xl p-10'>
-          <p className='text-justify pr-10 sm:text-xl text-base'>
+    <div className='h-full bg-[#1B1B1E] pt-14 flex flex-col'>
+      <main className='md:px-20 md:pb-10 p-6 h-full'>
+        <article className='flex flex-col h-full md:flex-row items-center bg-white rounded-xl p-10'>
+          <p className='text-justify md:px-20 md:text-3xl text-base'>
             Welcome to my personalized webpage! Delve into a treasure trove
             of information about me. Feel free to explore to your heart's 
             content and discover the various facets that make up who I am.
             Whether you're here for a casual browse or a deep dive, I hope
             you find something intriguing and enjoyable. Happy exploring!
           </p>
-          <img src={landinImg1} className='sm:w-1/2 w-full h-1/2'/>
+          <img src={landinImg1} className='md:w-1/3 w-full '/>
           {/* <a href="https://lovepik.com/images/png-web.html">Web Png vectors by Lovepik.com</a> */}
-          
         </article>
       </main>
       <aside className='w-full h-1/2 bg-[#373F51] flex flex-col items-center justify-center gap-4 pb-8'>
-        <p className='sm:text-2xl text-xl text-white py-4 w-3/4 sm:w-1/3 border-b-2 border-white text-center'>
-          Start exploring my webpage 🐱‍👤
+        <p className='md:text-2xl text-xl text-white py-4 w-3/4 md:w-1/3 border-b-2 border-white text-center'>
+          Start exploring my webpage 💻
         </p>
-        <a href="/Mywebself/#/about" class="text-xl overflow-hidden rounded relative inline-flex group items-center justify-center px-14 py-2 m-1 cursor-pointer border-b-4 border-l-2 active:border-[#58A4B0] active:shadow-none shadow-lg bg-gradient-to-tr from-[#58A4B0] to-[#58A4B0] border-[#3B747D] text-white">
-          <span class="absolute w-0 h-0 transition-all duration-300 ease-out bg-white rounded-full group-hover:w-32 group-hover:h-32 opacity-10"></span>
-          <span class="relative">Go to About </span>
-        </a>
+        <NavLink to="/about" className="text-xl overflow-hidden rounded relative inline-flex group items-center justify-center px-14 py-2 m-1 cursor-pointer border-b-4 border-l-2 active:border-[#58A4B0] active:shadow-none shadow-lg bg-gradient-to-tr from-[#58A4B0] to-[#58A4B0] border-[#3B747D] text-white">
+          <span className="absolute w-0 h-0 transition-all duration-300 ease-out bg-white rounded-full group-hover:w-32 group-hover:h-32 opacity-10"></span>
+          <span className="relative">Go to About </span>
+        </NavLink>
       </aside>
     </div>
   )
