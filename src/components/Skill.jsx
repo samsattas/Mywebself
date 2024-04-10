83a9c138a5dd114b39a24fbef03e4c1df101da9c
Samuel Satizabal <samuelsatizabal@hotmail.com>
2024-04-10 03:29:12 -0500

    FEAT: languages and more skills

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -1,12 +1,16 @@
-const Skill = ({name, value = 0}) => {
+const Skill = ({ name, value }) => {
   return (
     <li>
-      <div className='flex justify-between w-full bg-gray-200 rounded-full'>
-        <p className={`bg-[#58A4B0] text-white rounded-full px-4 flex justify-between`} style={{width: `${value}%`}}>
-          <div>{name}</div>{value}%
-          </p>
+      <div className="flex justify-between w-full bg-gray-200 rounded-full">
+        <p
+          className={`bg-[#58A4B0] text-white rounded-full px-4 flex justify-between`}
+          style={{ width: `${value ? value : 100}%` }}
+        >
+          <div>{name}</div>
+          {value && `${value}%`}
+        </p>
       </div>
     </li>
-  )
-}
+  );
+};
 export default Skill;
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,58 +1,114 @@
-import profile from '../assets/Profile.png'
-import Skill from '../components/Skill';
+import profile from "../assets/Profile.png";
+import Skill from "../components/Skill";
 
 const About = () => {
-  return(
-    <div className='h-full bg-[#1B1B1E] pt-14 flex flex-col'>
-      <main className='sm:px-20 sm:pb-10 p-6 h-full flex flex-col gap-8'>
-        <article className='flex flex-col h-fit md:flex-row items-center gap-6 md:gap-16 bg-white rounded-xl p-5 md:p-10 pb-16'>
-          <h2 className='text-[4vh] text-center md:text-start font-bold w-full border-b-[#1B1B1E] border-b-2 block md:hidden'>About me</h2>
-          <img src={profile} className='rounded-full w-2/3 md:w-1/3 lg:w-1/4 md:rounded-xl md:drop-shadow-[30px_30px_1px_#A9BCD0]' />
-          <p className='text-justify text-[2.3vh] w-full'>
-            <h2 className='text-[4vh] text-center md:text-start font-bold w-full border-b-[#1B1B1E] border-b-2 hidden md:block'>About me</h2>
-            <br/>
-            I am a junior motivated Front-end Developer, certified as Software System Engineer, with 
-            expertise in <b>HTML, CSS, JavaScript, React, Sass,</b> and best practices such as <b>accessibility,
-            mobile-first, responsive design, atomic design and BEM</b>. Passionate about creating intuitive
-            and visually appealing user experiences. Strong problem-solving abilities and a detail-oriented
-            approach to deliver high-quality code. <br />
-            During my degree I had the opportunity to develop Backend projects, so I have knowledge in <b>design
-            patterns, UML diagrams, unit tests, integration tests</b> and in languages such as <b>Java, Javascript and C#</b>.<br />
-            I embrace my identity as a highly creative individual, constantly venturing beyond conventional 
-            boundaries. This distinctive perspective enables me to grasp the broader scope of situations and offer 
-            innovative ideas that were previously unexplored.
+  return (
+    <div className="h-full bg-[#1B1B1E] pt-14 flex flex-col">
+      <main className="sm:px-20 sm:pb-10 p-6 h-full flex flex-col gap-8">
+        <article className="flex flex-col h-fit md:flex-row items-center gap-6 md:gap-16 bg-white rounded-xl p-5 md:p-10 pb-16">
+          <h2 className="text-[4vh] text-center md:text-start font-bold w-full border-b-[#1B1B1E] border-b-2 block md:hidden">
+            About me
+          </h2>
+          <img
+            src={profile}
+            className="rounded-full w-2/3 md:w-1/3 lg:w-1/4 md:rounded-xl md:drop-shadow-[30px_30px_1px_#A9BCD0]"
+          />
+          <p className="text-justify text-[2.3vh] w-full">
+            <h2 className="text-[4vh] text-center md:text-start font-bold w-full border-b-[#1B1B1E] border-b-2 hidden md:block">
+              About me
+            </h2>
+            <br />I am a full-stack developer, certified as a systems engineer,
+            with experience in{" "}
+            <b>C#, Java, JavaScript, React, Tailwind, SQL, git, Sass</b>, and
+            best practices such as{" "}
+            <b>
+              accessibility, mobile-first, responsive design, atomic design, and
+              BEM
+            </b>
+            . Passionate about creating intuitive and visually appealing user
+            experiences.{" "}
+            <b>Strong problem-solving skills and a detail-oriented approach</b>{" "}
+            to delivering high-quality code. I consider myself a highly
+            analytical, creative person with the <b>ability to learn quickly</b>{" "}
+            and efficiently.
           </p>
         </article>
-        <article className='flex flex-col h-fit items-center gap-6  bg-white rounded-xl p-5 md:p-10 pb-16'>
-          <h2 className='text-[4vh] text-center md:text-start font-bold w-full border-b-[#1B1B1E] border-b-2 block '>Skills</h2>
-          <div className='w-full flex gap-6 flex-col md:flex-row'>
-          <div className='flex flex-col gap-4 w-full'>
-            <h3 className='text-[3vh] font-bold'>Front-end</h3>
-            <ul className='flex flex-col gap-2'>
-              <Skill name='HTML' value={80} />
-              <Skill name='CSS' value={75} />
-              <Skill name='JavaScript' value={65} />
-              <Skill name='React' value={80} />
-              <Skill name='Sass' value={70} />
-            </ul>
+        <article className="flex flex-col h-fit items-center gap-6  bg-white rounded-xl p-5 md:p-10 pb-16">
+          <h2 className="text-[4vh] text-center md:text-start font-bold w-full border-b-[#1B1B1E] border-b-2 block ">
+            Skills
+          </h2>
+          <div className="w-full flex gap-6 flex-col lg:flex-row">
+            <div className="flex flex-col gap-4 w-full">
+              <h3 className="text-[3vh] font-bold">Front-end</h3>
+              <ul className="flex flex-col gap-2">
+                <Skill name="HTML" value={80} />
+                <Skill name="CSS" value={75} />
+                <Skill name="JavaScript" value={70} />
+                <Skill name="React" value={85} />
+                <Skill name="Sass" value={65} />
+                <Skill name="Tailwind" value={80} />
+                <Skill name="Next.js" value={50} />
+                <Skill name="UX" value={70} />
+                <Skill name="React native" value={50} />
+                <Skill name="Responsive design" value={90} />
+                <Skill name="Jest" value={40} />
+              </ul>
+            </div>
+            <div className="flex flex-col gap-4 w-full">
+              <h3 className="text-[3vh] font-bold">Back-end</h3>
+              <ul className="flex flex-col gap-2">
+                <Skill name="Java" value={60} />
+                <Skill name="C#" value={60} />
+                <Skill name="Spring" value={50} />
+                <Skill name="SQL" value={70} />
+                <Skill name="Node" value={30} />
+                <Skill name="AWS S3" value={60} />
+                <Skill name="AWS codecommit" value={60} />
+                <Skill name="Git" value={85} />
+                <Skill name="JUnit" value={50} />
+              </ul>
+            </div>
           </div>
-          <div className='flex flex-col gap-4 w-full'>
-            <h3 className='text-[3vh] font-bold'>Back-end</h3>
-            <ul className='flex flex-col gap-2'>
-              <Skill name='Java' value={60} />
-              <Skill name='C#' value={60} />
-              <Skill name='Spring' value={50} />
-              <Skill name='SQL' value={70} />
-              <Skill name='Node' value={30} />
-              <Skill name='AWS S3' value={60} />
-              <Skill name='AWS codecommit' value={60} />
-            </ul>
+          <div className="w-full flex gap-6 flex-col lg:flex-row">
+            <div className="flex flex-col gap-4 w-full">
+              <h3 className="text-[3vh] font-bold">Other skills</h3>
+              <ul className="flex flex-col gap-2">
+                <Skill name="Agile methodologies" />
+                <Skill name="Adaptability" />
+                <Skill name="Attention to detail" />
+                <Skill name="Creative" />
+                <Skill name="Perceptive" />
+                <Skill name="Self-motivated" />
+                <Skill name="Assertive communication" />
+              </ul>
+            </div>
+            <div className="flex flex-col gap-4 w-full">
+              <h3 className="text-[3vh] font-bold">Languages</h3>
+              <ul className="flex flex-col gap-2">
+                <Skill name="English: C1" />
+                <Skill name="Spanish: Native" />
+              </ul>
+            </div>
           </div>
-          </div>
+        </article>
+        <article className="flex flex-col h-fit items-center gap-6  bg-white rounded-xl p-5 md:p-10 pb-16">
+          <h2 className="text-[4vh] text-center md:text-start font-bold w-full border-b-[#1B1B1E] border-b-2 block ">
+            Education
+          </h2>
+          <p className="w-full flex flex-col gap-4">
+            <span>
+              <b>Systems Engineer degree</b>
+              <br /> - Universidad ICESI, Colombia (2018-2023)
+            </span>
+            <span>
+              <b>Student exchange</b>
+              <br /> - Goose Creek, SC, United States
+            </span>
+          </p>
         </article>
       </main>
     </div>
-  )
-}
+  );
+};
 
 export default About;
